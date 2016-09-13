c7fd54e9d0cd2d400ab6eed5bdd92ee135f5fe83
Pieter <pieter@pieterzoektbaan.nl>
2016-09-13 13:25:31 +0200

    Part 3: Multiple Components

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,7 @@ import { AppComponent }  from './app.component';
   ],
   declarations: [
 	AppComponent
+	HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
